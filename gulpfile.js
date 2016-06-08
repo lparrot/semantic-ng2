9a1617cddf3a2fffd0a1412b303f73f30ea370d1
Laurent PARROT <laurent.parrot78@gmail.com>
2016-06-08 10:34:28 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ gulp.task('copy', ['copy:fonts', 'copy:images']);
 gulp.task('clean', function () {
     return del.sync([
         'build/**/*',
-        // 'node_modules/**/*',
+        'node_modules/**/*',
         'typings/**/*',
         'vendors/**/*'
     ])
